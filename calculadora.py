13731db3095ba6836c8435467a6df0eaeabdb421
JhonIcc <stiven.stilex@gmail.com>
2025-04-22 14:34:06 +0200

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,27 @@
+# Pide dos números y una operación (+, -, *, /) al usuario.
+# Muestra el resultado según la operación que elija.
+
+print("****Calculadora****")
+print("1.- Suma")
+print("2.- Resta")
+print("3.- Multiplicacion")
+print("4.- Division")
+print("5.- Salir")
+
+option = int(input("Elije que deseas hacer: "))
+num1 = int(input("Elije el primer numero: "))
+num2 = int(input("Elije el segundo numero: "))
+
+if option == 1:
+    print(num1 + num2)
+elif option == 2:
+    print(num1 - num2)
+elif option == 3:
+    print(num1 * num2)
+elif option == 4:
+    print(num1 / num2)
+elif option == 5:
+    print("Hasta luego :))")
+else:
+    print("Introduce una opcion valida")
+
