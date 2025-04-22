d26a3f67424373bbe14f19a3a963944a3c37162f
JhonIcc <stiven.stilex@gmail.com>
2025-04-22 15:10:59 +0200

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -8,31 +8,24 @@ print("3.- Multiplicacion")
 print("4.- Division")
 print("5.- Salir")
 
-option = int(input("Elije que deseas hacer: "))
+option = int(input("Elige qué deseas hacer: "))
 
-if option == 1:
-    num1 = int(input("Elije el primer numero: "))
-    num2 = int(input("Elije el segundo numero: "))
-    print("Resultado:", num1 + num2)
-elif option == 2:
-    num1 = int(input("Elije el primer numero: "))
-    num2 = int(input("Elije el segundo numero: "))
-    print("Resultado:", num1 - num2)
-elif option == 3:
-    num1 = int(input("Elije el primer numero: "))
-    num2 = int(input("Elije el segundo numero: "))
-    print("Resultado:", num1 * num2)
-elif option == 4:
-    num1 = int(input("Elije el primer numero: "))
-    num2 = int(input("Elije el segundo numero: "))
-    # Doble if para que no se pueda dividir entre 0
-    if num2 == 0:
-        print("No se puede dividir entre 0")
-    else:
-        print("Resultado:", num1/num2)
-elif option == 5:
+if option == 5:
     print("Hasta luego :))")
+elif option >= 1 and option <= 4:
+    num1 = int(input("Elige el primer número: "))
+    num2 = int(input("Elige el segundo número: "))
+
+    if option == 1:
+        print("Resultado:", num1 + num2)
+    elif option == 2:
+        print("Resultado:", num1 - num2)
+    elif option == 3:
+        print("Resultado:", num1 * num2)
+    elif option == 4:
+        if num2 == 0:
+            print("No se puede dividir entre cero")
+        else:
+            print("Resultado:", num1 / num2)
 else:
-    print("Introduce una opcion valida")
-
-
+    print("Opción no válida. Intenta otra vez")
